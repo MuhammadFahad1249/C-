12794a4790cd6c624a999d030dd720ca013579b3
Muhammad Fahad <92125039+MuhammadFahad1249@users.noreply.github.com>
2024-08-13 23:29:48 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.Write("This is Engineer ");
+            Console.WriteLine("Muhammad Fahad");
+           
+            Console.ReadLine();
+            
+        }
+    }
+}
